7d28dfbb2ba1debbfe9689c5063e78c9fd677ac9
joshua-alkins <55319780+joshua-alkins@users.noreply.github.com>
2021-01-20 22:40:44 +0800

    functionality up to 39

--- RomanNumeral.py
+++ RomanNumeral.py
@@ -1,2 +1,26 @@
-def convertToRomanNumerals(i):
-    return
+# I=1, V=5, X=10, L=50, C=100, D=500, M=1000
+
+def convertToRomanNumerals(n):
+    if type(n) != int:
+        raise TypeError("Input must be an integer!")
+
+    conversions = [(50,'L'),(10,'X'),(5,'V'),(1,'I')]
+
+    result = ''
+    carry = n
+
+    for i in range(len(conversions)):
+        num = conversions[i][0]
+        numeral = conversions[i][1]
+
+        number_of_numerals = carry // num 
+        carry = carry%num
+
+        result += numeral * number_of_numerals
+
+        if carry == num-1 and carry != 0 and num <= 10:
+            carry = 0
+            result += 'I'+numeral
+
+
+    return result
--- Test_RomanNumerals.py
+++ Test_RomanNumerals.py
@@ -1,10 +1,56 @@
 import unittest
 from RomanNumeral import convertToRomanNumerals
+import math
 
 class RomanNumeralTestMethods(unittest.TestCase):
-    def testFirstDigit(self):
-        self.assetEqual(convertToRomanNumerals(1),'I')
+    def testTypeError(self):
+        self.assertRaises(TypeError,convertToRomanNumerals,'I')
+        self.assertRaises(TypeError,convertToRomanNumerals,0.11)
+        self.assertRaises(TypeError,convertToRomanNumerals,1j)
 
+    def testZero(self):
+        self.assertEquals(convertToRomanNumerals(0),'')
 
+    def testI(self):
+        self.assertEquals(convertToRomanNumerals(1),'I')
+        self.assertEquals(convertToRomanNumerals(2),'II')
+
+    def testIV(self):
+        self.assertEquals(convertToRomanNumerals(4),'IV')
+
+    def testV(self):
+        self.assertEquals(convertToRomanNumerals(5),'V')
+        self.assertEquals(convertToRomanNumerals(8),'VIII')
+
+    def testIX(self):
+        self.assertEquals(convertToRomanNumerals(9),'IX')
+
+    def testX(self):
+        self.assertEquals(convertToRomanNumerals(10),'X')
+        self.assertEquals(convertToRomanNumerals(13),'XIII')
+        self.assertEquals(convertToRomanNumerals(14),'XIV')
+        self.assertEquals(convertToRomanNumerals(15),'XV')
+        self.assertEquals(convertToRomanNumerals(18),'XVIII')
+        self.assertEquals(convertToRomanNumerals(19),'XIX')
+    
+    def testXX(self):
+        self.assertEquals(convertToRomanNumerals(20),'XX')
+        self.assertEquals(convertToRomanNumerals(24),'XXIV')
+        self.assertEquals(convertToRomanNumerals(25),'XXV')
+        self.assertEquals(convertToRomanNumerals(28),'XXVIII')
+        self.assertEquals(convertToRomanNumerals(29),'XXIX')
+        self.assertEquals(convertToRomanNumerals(30),'XXX')
+
+    def test40(self):
+        self.assertEquals(convertToRomanNumerals(40),'XL')
+
+    def testL(self):
+        self.assertEquals(convertToRomanNumerals(50),'L')
+        self.assertEquals(convertToRomanNumerals(52),'LII')
+        self.assertEquals(convertToRomanNumerals(54),'LIV')
+        self.assertEquals(convertToRomanNumerals(55),'LV')
+        self.assertEquals(convertToRomanNumerals(57),'LVII')
+        self.assertEquals(convertToRomanNumerals(59),'LIX')
+    
 if __name__ == '__main__':
     unittest.main()
